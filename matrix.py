ef446e90f4dba8a2d163479918f6a81cd315f6fa
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2022-07-24 18:01:34 +0900

    update

--- matrix.py
+++ matrix.py
@@ -1,116 +1,36 @@
-mod = 10**9 + 7
+mod = 998244353
 
+# transpose([[1,2],[3,4]]) == [[1,3],[2,4]]
+def transpose(a):
+    res = [[0] * len(a) for _ in range(len(a[0]))]
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            res[j][i] = a[i][j]
+    return res
 
-def mat_transpose(A):
-    B = [[0] * len(A) for _ in range(len(A[0]))]
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            B[j][i] = A[i][j]
-    return B
+# dot_t(a, b) == a @ b.T
+def dot_t(a, b):
+    assert len(a[0]) == len(b[0])
+    res = [[0] * len(b) for _ in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(b)):
+            for k in range(len(a[0])):
+                res[i][j] += a[i][k] * b[j][k] % mod
+            res[i][j] %= mod
+    return res
 
+# dot(a, b) == a @ b
+def dot(a, b):
+    return dot_t(a, transpose(b))
 
-def mat_flatten(A):
-    B = [0] * (len(A) * len(A[0]))
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            B[len(A[0])*i + j] = A[i][j]
-    return B
-
-
-def vec_mul_vec(x, y):
-    assert len(x) == len(y)
-    z = [0] * len(x)
-    for i in range(len(x)):
-        z[i] = x[i] * y[i]
-    return z
-
-
-def mat_mul_vec(A, x):
-    assert len(A[0]) == len(x)
-
-    y = [0] * len(A)
-    for i in range(len(A)):
-        tmp = 0
-        for j in range(len(x)):
-            tmp += A[i][j] * x[j]
-        y[i] = tmp
-
-    return y
-
-
-def mat_mul_mat(A, B):
-    assert len(A[0]) == len(B)
-
-    C = [[0] * len(B[0]) for _ in range(len(A))]
-
-    for i in range(len(A)):
-        A_i = A[i]
-        for j in range(len(B[0])):
-            tmp = 0
-            for k in range(len(B)):
-                tmp += A_i[k] * B[k][j]
-                tmp %= mod###
-            C[i][j] = tmp
-
-    return C
-
-
-from copy import deepcopy
-def mat_add_mat(A, B):
-    assert len(A) == len(B) and len(A[0]) == len(B[0])
-    C = deepcopy(A)
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            C[i][j] += B[i][j]
-    return C
-
-
-def mat_power(A, exp):
-    assert len(A) == len(A[0])
-
-    dbl = [A]
-    for i in range(exp.bit_length()-1):
-        dbl.append(mat_mul_mat(dbl[i],dbl[i]))
-
-    B = [[0] * len(A) for _ in range(len(A))]
-    for i in range(len(A)):
-        B[i][i] = 1
-
-    for i in range(exp.bit_length()):
+def matrix_power(a, exp):
+    n = len(a)
+    a = transpose(a)
+    res = [[0] * n for _ in range(n)]
+    for i in range(n):
+        res[i][i] = 1
+    for i in reversed(range(exp.bit_length())):
+        res = dot(res, res)
         if exp>>i & 1:
-            B = mat_mul_mat(B, dbl[i])
-
-    return B
-
-
-### test ###
-import numpy as np
-
-A = np.random.randint(1, 10, (3,4))
-B = np.random.randint(1, 10, (4,2))
-C = np.random.randint(1, 10, (3,3))
-D = np.random.randint(1, 10, (3,4))
-x = np.random.randint(1, 10, 4)
-y = np.random.randint(1, 10, 4)
-
-def ndarray_to_list(A):
-    if A.ndim == 1:
-        return list(A)
-    if A.ndim == 2:
-        return [list(row) for row in A]
-
-A_lis = ndarray_to_list(A)
-B_lis = ndarray_to_list(B)
-C_lis = ndarray_to_list(C)
-D_lis = ndarray_to_list(D)
-x_lis = ndarray_to_list(x)
-y_lis = ndarray_to_list(y)
-
-print(ndarray_to_list(A.T) == mat_transpose(A_lis))
-print(ndarray_to_list(A.flatten()) == mat_flatten(A_lis))
-print(ndarray_to_list(x * y) == vec_mul_vec(x_lis, y_lis))
-print(ndarray_to_list(A @ x) == mat_mul_vec(A_lis, x_lis))
-print(ndarray_to_list(A @ B) == mat_mul_mat(A_lis, B_lis))
-print(ndarray_to_list(A + D) == mat_add_mat(A_lis, D_lis))
-print(ndarray_to_list(np.linalg.matrix_power(C, 5)) == mat_power(C_lis, 5))
-
+            res = dot_t(res, a)
+    return res
