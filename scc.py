7f4ddc3205dca77c8ed0e8f0cf83a3db01d284e9
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2022-07-28 17:51:50 +0900

    update

--- scc.py
+++ scc.py
@@ -1,44 +1,72 @@
-def scc(edges):
-    n = len(edges)
-    ids = [len(edges[u]) for u in range(n)]
-    visited = [0] * n
-    ord = [0] * n
-    label = n
-    for u in range(n):
-        if visited[u]:
-            continue
-        visited[u] = 1
-        path = [u]
-        while path:
-            u = path[-1]
-            if ids[u]:
-                ids[u] -= 1
-                v = edges[u][ids[u]]
-                if not visited[v]:
-                    visited[v] = 1
-                    path.append(v)
+from sys import setrecursionlimit
+setrecursionlimit(1000000000)
+
+class StronglyConnectedComponents:
+    def __init__(self, n):
+        self.n = n
+        self.edges = []
+
+    def add_edge(self, fr, to):
+        self.edges.append((fr, to))
+
+    def csr(self):
+        # Compressed Sparse Row
+        self.start = [0] * (self.n + 1)
+        for u, _ in self.edges:
+            self.start[u + 1] += 1
+        for i in range(self.n):
+            self.start[i+1] += self.start[i]
+        counter = self.start.copy()
+        self.elist = [0] * len(self.edges)
+        for u, v in self.edges:
+            self.elist[counter[u]] = v
+            counter[u] += 1
+
+    def dfs(self, v):
+        self.low[v] = self.ord[v] = self.now_ord
+        self.now_ord += 1
+        self.visited[self.idx] = v
+        self.idx += 1
+        for i in range(self.start[v], self.start[v+1]):
+            to = self.elist[i]
+            if self.ord[to] == -1:
+                self.dfs(to)
+                self.low[v] = min(self.low[v], self.low[to])
             else:
-                label -= 1
-                ord[label] = u
-                path.pop()
-    r_edges = [[] for _ in range(n)]
-    for u in range(n):
-        for v in edges[u]:
-            r_edges[v].append(u)
-    groups = []
-    visited = [0] * n
-    for u in ord:
-        if visited[u]:
-            continue
-        visited[u] = 1
-        group = [u]
-        idx = 0
-        while idx < len(group):
-            u = group[idx]
-            idx += 1
-            for v in r_edges[u]:
-                if not visited[v]:
-                    visited[v] = 1
-                    group.append(v)
-        groups.append(group)
-    return groups
+                self.low[v] = min(self.low[v], self.ord[to])
+        if self.low[v] == self.ord[v]:
+            while True:
+                self.idx -= 1
+                u = self.visited[self.idx]
+                self.ord[u] = self.n
+                self.ids[u] = self.group_num
+                if u == v:
+                    break
+            self.group_num += 1
+
+    def scc_ids(self):
+        self.csr()
+        self.now_ord = self.group_num = 0
+        self.visited = [0] * self.n
+        self.idx = 0
+        self.low = [0] * self.n
+        self.ord = [-1] * self.n
+        self.ids = [0] * self.n
+        for i in range(self.n):
+            if self.ord[i] == -1:
+                self.dfs(i)
+        for i in range(self.n):
+            self.ids[i] = self.group_num - 1 - self.ids[i]
+
+    def scc(self):
+        self.scc_ids()
+        counts = [0] * self.group_num
+        for x in self.ids:
+            counts[x] += 1
+        groups = [[] for _ in range(self.group_num)]
+        for i in range(self.group_num):
+            groups[i] = [0] * counts[i]
+        for i in reversed(range(self.n)):
+            counts[self.ids[i]] -= 1
+            groups[self.ids[i]][counts[self.ids[i]]] = i
+        return groups
