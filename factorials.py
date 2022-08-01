61de30e93cbe13bba9ec6c1dd2226a134436f2bb
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2022-08-01 11:16:16 +0900

    update

--- factorials.py
+++ factorials.py
@@ -2,34 +2,43 @@ def comb(n, r):
     r = min(r, n-r)
     res = 1
     for i in range(r):
-        res *= n-i
-        res //= i+1
+        res *= n - i
+        res //= i + 1
     return res
 
 
 class Factorials:
     def __init__(self, n, mod):
+        self.n = n + 1
         self.mod = mod
 
-        # self.fac[i] ≡ i!
-        self.fac = [1] * (n+1)
-        for i in range(2, n+1):
-            self.fac[i] = self.fac[i-1] * i % mod
+        # self.f[i] ≡ i!
+        self.f = [1] * self.n
+        for i in range(2, self.n):
+            self.f[i] = self.f[i-1] * i % mod
 
-        # self.rec[i] ≡ 1 / i!
-        self.rec = [1] * (n+1)
-        self.rec[n] = pow(self.fac[n], mod-2, mod)
-        for i in range(n-1, 1, -1):
-            self.rec[i] = self.rec[i+1] * (i+1) % mod
+        # self.g[i] ≡ 1 / i!
+        self.g = [0] * self.n
+        self.g[n] = pow(self.f[n], mod-2, mod)
+        for i in reversed(range(n)):
+            self.g[i] = self.g[i+1] * (i+1) % mod
 
-    # self.comb(n, r) ≡ nCr
+    # self.fact(n) ≡ n!
+    def fact(self, n):
+        return self.f[n]
+
+    # self.fact_inv(self, n) ≡ 1 / n!
+    def fact_inv(self, n):
+        return self.g[n]
+
+    # self.comb(n, r) ≡ nCr ≡ n! / (r! * (n-r)!)
     def comb(self, n, r):
-        return self.fac[n] * self.rec[r] * self.rec[n-r] % self.mod
+        return self.f[n] * self.g[r] % self.mod * self.g[n-r] % self.mod
     
-    # self.perm(n, r) ≡ nPr
+    # self.perm(n, r) ≡ nPr ≡ n! / (n-r)!
     def perm(self, n, r):
-        return self.fac[n] * self.rec[n-r] % self.mod
+        return self.f[n] * self.g[n-r] % self.mod
 
     # self.inv(n) ≡ 1 / n ≡ pow(n, mod-2, mod)
     def inv(self, n):
-        return self.fac[n-1] * self.rec[n] % self.mod
+        return self.f[n-1] * self.g[n] % self.mod
