70b6aa43a3eb8578581efa6f42cd6db8f0b4a689
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2022-08-01 22:50:15 +0900

    update

--- pivottree.py
+++ pivottree.py
@@ -7,15 +7,28 @@ class PivotTree:
         self.n = n
         self.root = self.node(1<<n, 1<<n)
 
-    def add(self, v): # vを追加する. vをすでに含む場合はエラー.
+    class node:
+        def __init__(self, v, p):
+            self.value = v
+            self.pivot = p
+            self.left = None
+            self.right = None
+            self.size = 1
+
+    def insert(self, v):
+        """
+        It adds an element v.
+        It returns -1 if the tree already contains v, 0 otherwise.
+        """
+        assert 0 < v + 1 < self.root.value
         v += 1
         nd = self.root
-
         while True:
-            assert v != nd.value
-            nd.size += 1            
-            
-            mi, ma = min(v,nd.value), max(v,nd.value)
+            if v == nd.value:
+                return -1
+            nd.size += 1
+            mi = min(v, nd.value)
+            ma = max(v, nd.value)
             if mi < nd.pivot:
                 nd.value = ma
                 if nd.left:
@@ -24,7 +37,7 @@ class PivotTree:
                 else:
                     p = nd.pivot
                     nd.left = self.node(mi, p - (p&-p)//2)
-                    break
+                    return 0
             else:
                 nd.value = mi
                 if nd.right:
@@ -33,19 +46,23 @@ class PivotTree:
                 else:
                     p = nd.pivot
                     nd.right = self.node(ma, p + (p&-p)//2)
-                    break
-    
+                    return 0
+
     def leftmost(self, nd):
         if nd.left:
             return self.leftmost(nd.left)
         return nd
-    
+
     def rightmost(self, nd):
         if nd.right:
             return self.rightmost(nd.right)
         return nd
-    
-    def remove(self, v, nd=None, prev=None): # 値がvのノードを削除する. 値がvのノードがなければ-1を返す.
+
+    def erase(self, v, nd=None, prev=None):
+        """
+        It removes an element v.
+        It returns 0 if the tree contains v, -1 otherwise.
+        """
         v += 1
         if not nd:
             nd = self.root
@@ -64,7 +81,6 @@ class PivotTree:
                     nd = nd.right
                 else:
                     return -1
-
         nd.size -= 1
         if not nd.left and not nd.right:
             if not prev.left:
@@ -77,21 +93,27 @@ class PivotTree:
                 prev.right = None
         elif nd.right:
             nd.value = self.leftmost(nd.right).value
-            self.remove(nd.value-1, nd.right, nd)
+            self.erase(nd.value - 1, nd.right, nd)
         else:
             nd.value = self.rightmost(nd.left).value
-            self.remove(nd.value-1, nd.left, nd)
-    
-    def nth(self, k): # 0-indexed で小さいほうからk番目の値を取得する.
-        assert k < self.root.size - 1
-        nd = self.root
+            self.erase(nd.value - 1, nd.left, nd)
 
+    def size(self):
+        return self.root.size - 1
+
+    def find_by_order(self, k):
+        """
+        It returns k-th value in ascending order with 0-indexed.
+        It returns -1 if the size is smaller than k.
+        """
+        if self.size() <= k:
+            return -1
+        nd = self.root
         while True:
             if nd.left:
                 left_size = nd.left.size
             else:
                 left_size = 0
-            
             if k < left_size:
                 nd = nd.left
             elif k == left_size:
@@ -99,16 +121,4 @@ class PivotTree:
             else:
                 k -= left_size + 1
                 nd = nd.right
-
         return nd.value - 1
-
-    def size(self):
-        return self.root.size - 1
-
-    class node:
-        def __init__(self, v, p):
-            self.value = v
-            self.pivot = p
-            self.left = None
-            self.right = None
-            self.size = 1
